4bd77387e2c57a1b1d3e280690e50de601f1ae44
Conal Elliott <conal@conal.net>
2013-03-13 12:58:34 -0700

    refactored & tidied

--- src/Data/Boolean/Numbers.hs
+++ src/Data/Boolean/Numbers.hs
@@ -1,10 +1,9 @@
-
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE FlexibleContexts #-}
 
 -- There are warnings about this in the 'RealFracB' instances for 
 -- 'Float' and 'Double'. They can be ignored.
-{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+{-# OPTIONS_GHC -Wall -fno-warn-type-defaults #-}
 
 -- -----------------------------------------------------------------------
 -- |
@@ -31,7 +30,25 @@ module Data.Boolean.Numbers
   , evenB, oddB
   ) where
 
+import Prelude as P
 import Data.Boolean
+
+{--------------------------------------------------------------------
+    Misc
+--------------------------------------------------------------------}
+
+infixr 9 .:
+(.:) :: (c -> c') -> (a -> b -> c) -> (a -> b -> c')
+(.:) = (.).(.)
+
+(##) :: (a -> b -> c) -> (a -> b -> d) -> a -> b -> (c,d)
+(f ## g) x y = (f x y, g x y)
+
+viaZ :: (Integral a, Num b) => a -> b
+viaZ = fromInteger . toInteger
+
+-- Why not fromIntegral instead of fromInteger . toInteger ? (conal)
+-- Why use it at all? (conal)
 
 -- -----------------------------------------------------------------------
 -- Generalized Number Class Hirarchy
@@ -45,26 +62,24 @@ import Data.Boolean
 class (Num a, OrdB a) => IntegralB a where
   -- | Integer division truncated towards zero.
   quot :: a -> a -> a
-  quot x y = fst $ Data.Boolean.Numbers.quotRem x y
+  quot = fst .: Data.Boolean.Numbers.quotRem
   -- | Integer reminder, satisfying:
   --   @(x `quot` y) * y + (x `rem` y) == x@
   rem :: a -> a -> a
-  rem x y = snd $ Data.Boolean.Numbers.quotRem x y
+  rem = snd .: Data.Boolean.Numbers.quotRem
   -- | Integer division truncated toward negative infinity.
   div :: a -> a -> a
-  div x y = fst $ Data.Boolean.Numbers.divMod x y
+  div = fst .: Data.Boolean.Numbers.divMod
   -- | Integer modulus, satisfying:
   --   @(x `div` y) * y + (x `mod` y) == x@
   mod :: a -> a -> a
-  mod x y = snd $ Data.Boolean.Numbers.divMod x y
+  mod = snd .: Data.Boolean.Numbers.divMod
   -- | Simultaneous 'quot' and 'rem'.
   quotRem :: a -> a -> (a,a)
-  quotRem x y = ( x `Data.Boolean.Numbers.quot` y
-                , x `Data.Boolean.Numbers.rem`  y)
+  quotRem = Data.Boolean.Numbers.quot ## Data.Boolean.Numbers.rem
   -- | Simultaneous 'div' and 'mod'.
   divMod :: a -> a -> (a,a)
-  divMod x y = ( x `Data.Boolean.Numbers.div` y
-               , x `Data.Boolean.Numbers.mod` y)
+  divMod  = Data.Boolean.Numbers.div  ## Data.Boolean.Numbers.mod
 
 -- | Deep embedded version of 'RealFloat'.
 --   Extracting components of fractions.
@@ -85,7 +100,7 @@ class (Num a, OrdB a, Fractional a) => RealFracB a where
   properFraction :: a -> (a, a)
   -- | @'truncate' x@ returns the integer nearest @x@ between zero and @x@
   truncate :: a -> a
-  truncate x = fst $ Data.Boolean.Numbers.properFraction x
+  truncate = fst . Data.Boolean.Numbers.properFraction
   -- | @'round' x@ returns the nearest integer to @x@;
   --   the even integer if @x@ is equidistant between two integers
   round :: a -> a
@@ -136,44 +151,38 @@ oddB = notB . evenB
 -- -----------------------------------------------------------------------
 
 instance IntegralB Int where
-  quotRem x y = (x `Prelude.quot` y, x `Prelude.rem` y)
-  divMod x y = (x `Prelude.div` y, x `Prelude.mod` y)
+  quotRem = P.quot ## P.rem
+  divMod  = P.div  ## P.mod
 
 instance IntegralB Integer where
-  quotRem x y = (x `Prelude.quot` y, x `Prelude.rem` y)
-  divMod x y = (x `Prelude.div` y, x `Prelude.mod` y)
+  quotRem = P.quot ## P.rem
+  divMod  = P.div  ## P.mod
+
+-- Why not quotRem = P.quotRem etc? (conal)
 
 instance RealFracB Float where
-  properFraction x = 
-    let (n, f) = Prelude.properFraction x
-    in (fromInteger $ toInteger n, f)
-  round = fromInteger . toInteger . Prelude.round
-  floor = fromInteger . toInteger . Prelude.floor
-  ceiling = fromInteger . toInteger . Prelude.ceiling
+  properFraction x = (viaZ n, f) where (n, f) = P.properFraction x
+  round   = viaZ . P.round
+  floor   = viaZ . P.floor
+  ceiling = viaZ . P.ceiling
 
 instance RealFracB Double where
-  properFraction x = 
-    let (n, f) = Prelude.properFraction x
-    in (fromInteger $ toInteger n, f)
-  round = fromInteger . toInteger . Prelude.round
-  floor = fromInteger . toInteger . Prelude.floor
-  ceiling = fromInteger . toInteger . Prelude.ceiling
+  properFraction x = (viaZ n, f) where (n, f) = P.properFraction x
+  round   = viaZ . P.round
+  floor   = viaZ . P.floor
+  ceiling = viaZ . P.ceiling
 
 instance RealFloatB Float where
-  isNaN = Prelude.isNaN
-  isInfinite = Prelude.isInfinite
-  isNegativeZero = Prelude.isNegativeZero
-  isIEEE = Prelude.isIEEE
-  atan2 = Prelude.atan2
+  isNaN          = P.isNaN
+  isInfinite     = P.isInfinite
+  isNegativeZero = P.isNegativeZero
+  isIEEE         = P.isIEEE
+  atan2          = P.atan2
 
 instance RealFloatB Double where
-  isNaN = Prelude.isNaN
-  isInfinite = Prelude.isInfinite
-  isNegativeZero = Prelude.isNegativeZero
-  isIEEE = Prelude.isIEEE
-  atan2 = Prelude.atan2
-
-
-
-
+  isNaN          = P.isNaN
+  isInfinite     = P.isInfinite
+  isNegativeZero = P.isNegativeZero
+  isIEEE         = P.isIEEE
+  atan2          = P.atan2
 
